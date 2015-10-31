91c788e9209e851bef28b21b21b450b31e884535
Milan Karunarathne <mhkarunarathne@gmail.com>
2015-10-31 15:13:39 +0530

    Updated npm package json
    - Installed dashboard packages for ReactJS
    - Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  entry: './app/App.js',
+  output: {
+    filename: 'public/bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader'
+    }]
+  }
+};
