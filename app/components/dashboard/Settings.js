15701c57cc42d8d2395f9aa847635ee4a7fa6916
Milan Karunarathne <mhkarunarathne@gmail.com>
2015-11-16 20:06:18 +0530

    Downgrade to Bootstrap-3 & implement Settings
    - Downgrade because of lack of device support in Bootstrap-4 and in
      Alpha version
    - Implement Settings with Bootstrap navbar

--- app/components/dashboard/Settings.js
+++ app/components/dashboard/Settings.js
@@ -9,26 +9,81 @@
 
 import React, { PropTypes } from 'react';
 
-const Settings = React.createClass({
+class Settings extends React.Component {
+
     render () {
         return (
             <div className="row">
-                <div className="col-md-5 col-md-offset-7">
-                    <div className="row">
-                        <h6 className="col-md-6 bold">
-                            Welcome Judy
-                        </h6>
-                        <button
-                            type="button"
-                            className="col-md-3 btn btn-primary-outline">Settings</button>
-                        <button
-                            type="button"
-                            className="col-md-3 btn btn-danger-outline">Logout</button>
+                <nav className="navbar navbar-default navbar-fixed-top">
+                    <div className="container-fluid">
+                        {this.renderHeader()}
+                        {this.renderLogout()}
                     </div>
-                </div>
+                </nav>
             </div>
         );
     }
-});
+
+    renderHeader() {
+        return (
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed"
+                  data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"
+                  aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">PHR</a>
+            </div>
+        );
+    }
+
+    renderLogout() {
+        return (
+            <div className="collapse navbar-collapse"  id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav navbar-right">
+                    <li>
+                        <a>Welcome Judy</a>
+                    </li>
+                    <li className="dropdown">
+                        <a href="#"
+                            className="dropdown-toggle" data-toggle="dropdown"
+                            role="button" aria-haspopup="true" aria-expanded="false">
+                            <img alt="@milankarunarathne" width="20" height="20"
+                                src="https://avatars0.githubusercontent.com/u/11207066?v=3&s=40"/>
+                            <span className="caret"></span>
+                        </a>
+                        <ul className="dropdown-menu">
+                            <li>
+                                <a>Sign in as <br/>
+                                    <strong>milankarunarathne</strong>
+                                </a>
+                            </li>
+                            <li role="separator" className="divider"></li>
+                            <li>
+                                <a href="#">Profile</a>
+                            </li>
+                            <li>
+                                <a href="#">Applications</a>
+                            </li>
+                            <li>
+                                <a href="#">Help</a>
+                            </li>
+                            <li role="separator" className="divider"></li>
+                            <li>
+                                <a href="#">Settings</a>
+                            </li>
+                            <li>
+                                <a href="#">Logout</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+}
 
 export default Settings;
