d50958d1a7a0b9cd8d659bd0580a3d22a7c75747
Milan Karunarathne <mhkarunarathne@gmail.com>
2016-01-14 00:32:22 +0530

    Implemented DashBoard Action to change password in server

--- app/actions/DashboardActions.js
+++ app/actions/DashboardActions.js
@@ -8,12 +8,32 @@
 "use strict";
 
 import axios from 'axios';
+import url from 'url';
+
+import BaseActions from '../apps/util/BaseActions';
 
 import PHRDispacher from '../dispatcher/PHRDispatcher';
 import DashboardConstants from '../constants/DashboardConstants';
 
-class DashboardActions {
-    constructor() {}
+class DashboardActions extends BaseActions {
+    constructor() {
+        super();
+    }
+
+    static changePassword(passowrd) {
+        console.log('DashboardActions changePassword : ', passowrd);
+        axios.post(`/user/pwChange/${this.getUser().username}`, passowrd, {
+            headers: this.getHeaders()
+        }).then(res => {
+            console.log('/dashboard changePassword : ', res);
+            PHRDispacher.dispatch({
+                actionType: DashboardConstants.PASSWORD_CHANGED
+            });
+        }).catch(err => {
+            console.error(err);
+
+        });
+    }
 }
 
 export default DashboardActions;
